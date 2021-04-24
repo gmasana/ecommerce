0454330328f10f64a3a55a0e4d903ab71442e5fc
gmasana <gonzalomasana95@gmail.com>
2021-04-24 15:35:54 -0300

    Footer basico

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
         <footer>
+            <section class="layout-f color-1">
+                <div>
+                    <p>Teléfono: <a href="tel:+">011-2222-3333</a></p>
+                    <p>Mail: <a href="#">mail@coloricaramela.com</a></p>
+                </div>
+                <div class="social-media">
+                    <p><a href="#facebook">Seguinos en Facebook: <i>Facebook</i></a></p>
+                    <p><a href="#instagram">Seguinos en Instagram: <i>Instagram</i></a></p>
+                </div>
+                <p class="copyright">Colori Caramela &copy;</p>
+            </section>
         </footer>
         <script src="js/animations.js" type="text/javascript"></script>
     </body>
